30e80988d5ed8c896c6d059e4e8a9b9f33f2bf18
Rajgupta152 <kshitij.anncode@gmail.com>
2024-01-13 18:05:08 +0530

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
     cors({
-        origin: ["https://food-app-client-tau.vercel.app/"],
+        origin: ["https://food-app-client-tau.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
 }));
